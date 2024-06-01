ff43caa098e31cb5d720802dbea9a569ac500550
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-06-01 21:12:44 +0300

    add ex.4 test task
    - добавил программу для выполнения упражнение №4 (контрольное) "Создание объектов"

---
+++ Lab09/Starter/TestTask/TestTask/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    { 
+        //--.
+        public static void Main(string[] args )
+        {
+            
+            //--. первая точка
+            Console.Write("Введите p1.x = ");
+            double pX = Convert.ToDouble( Console.ReadLine() );
+            Console.Write("Введите p1.y = ");
+            double pY = Convert.ToDouble( Console.ReadLine() );
+            TPoint p1 = new TPoint( pX, pY );
+
+            //--. вторая точка
+            Console.Write("Введите p2.x = ");
+            pX = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите p2.y = ");
+            pY = Convert.ToDouble(Console.ReadLine());
+            TPoint p2 = new TPoint(pX, pY);
+
+            //--. Третья точка
+            Console.Write("Введите p3.x = ");
+            pX = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите p3.y = ");
+            pY = Convert.ToDouble(Console.ReadLine());
+            TPoint p3 = new TPoint(pX, pY);
+
+
+            //--.
+            TTriangle tr = new TTriangle(p1, p2, p3);
+
+            //--. Расчёт периметра треугольника
+            double prRes = tr.Perimetr();
+
+            //--. Расчёт площади треугольника
+            double sqRes = tr.Square();
+
+            //--. Вывести длины сторон треугольника
+            var (side1Len, side2Len, side3Len) = tr.getSedeLength();
+
+
+            //--. Реализовать проверку, позволяющую установить, существует ли треугольник с данными сторонами
+            bool validTriangle = tr.Valid();
+
+            //--.
+            if (!validTriangle)
+            {
+                Console.WriteLine("Проверка сторон треугольника: Треугольника с такими сторонами НЕ СУЩЕСТВУЕТ!");
+            }
+            else
+            {
+                Console.WriteLine("Проверка сторон треугольника: треугольник сущесвует.");
+
+                //--.
+                Console.WriteLine("\tОтвет:");
+                Console.WriteLine("\t------------------");
+                Console.WriteLine("\tПериметр треуголиника равен  {0:F2}", prRes);
+                Console.WriteLine("\tПлощадь треуголиника равна  {0:F2}", sqRes);
+                Console.WriteLine("\tСтороны треугольника равны: сторона 1 =  {0:F2}, сторона 2 =  {1:F2}, сторона 3 =  {2:F2}", side1Len, side2Len, side3Len);
+
+            }
+
+        }
+
+    }
+}
---
+++ Lab09/Starter/TestTask/TestTask/TTriangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+
+public class TTriangle
+{
+    TPoint p1, p2, p3;
+
+    //--.
+    public TTriangle() 
+    {
+    }
+    public TTriangle(TPoint p1, TPoint p2, TPoint p3)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    //--.
+    public  (double sideLen1, double sideLen2, double sideLen3) getSedeLength()
+    {
+        double sideLen1 = p1.Distance(p2);
+        double sideLen2 = p2.Distance(p3);
+        double sideLen3 = p3.Distance(p1);
+
+        return (sideLen1, sideLen2, sideLen3);
+    }
+
+    //--.
+    public double Perimetr()
+    {
+        var resSlen = getSedeLength();
+        return resSlen.sideLen1 + resSlen.sideLen2 + resSlen.sideLen3;
+    }
+
+    //--.
+    public double Square()
+    {
+        var (a, b, c) = getSedeLength();
+
+        //--. полупериметр
+        double p = (a + b + c) / 2;
+
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    //--.
+    public bool Valid()
+    {
+        //--.
+        var (side1Len, side2Len, side3Len) = getSedeLength();
+
+            if ((side1Len + side2Len) <= side3Len ||
+                (side2Len + side3Len) <= side1Len ||
+                (side3Len + side1Len) <= side2Len)
+                return false;
+
+            return true;
+
+    }
+
+}

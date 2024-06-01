e8f4b782596089dd57dab31abb6fb47c187e44b9
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-06-01 19:07:03 +0300

    add ex.2 Init data readonly
    - добавил программу для выполнения упражнения №2 "Инициализация данных только для чтения"

---
+++ Lab09/Starter/ConstructorsBankTransactions/ConstructorsBankTransactions/BankAccount.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+
+class BankAccount
+{
+    long accNo;
+    decimal accBal;
+    AccountType accType;
+    //--.
+    static long nextAccNo = 123;
+
+    //--.
+    public BankAccount()
+    {
+        this.accNo = NextNumber();
+        this.accType = AccountType.Checking;
+        this.accBal = 0;
+    }
+
+    //--.
+    public BankAccount(AccountType aType)
+    {
+        this.accNo = NextNumber();
+        this.accType = aType;
+        this.accBal = 0;
+
+    }
+    
+    //--.
+    public BankAccount(decimal aBal)
+    {
+        this.accNo = NextNumber();
+        this.accType = AccountType.Checking;
+        this.accBal = aBal;
+    }
+    
+    //--.
+    public BankAccount(AccountType aType, decimal aBal)
+    {
+        this.accNo = NextNumber();
+        this.accType = aType;
+        this.accBal = aBal;
+    }
+
+
+
+    //--.
+    public long Number()
+    {
+        return accNo;
+    }
+
+    //--.
+    public decimal Ballance()
+    {
+        return accBal;
+    }
+
+    //--.
+    public string Type()
+    {
+        return accType.ToString();
+    }
+
+    //--.
+    static long NextNumber()
+    {
+        return nextAccNo++;
+    }
+
+    //--.
+    public decimal Deposit(decimal amount ) 
+    {
+        accBal += amount;
+        return accBal;
+    }
+
+    //--.
+    public bool Withdraw(decimal amount) 
+    {
+        //--.
+        bool sufficientFunds = accBal >= amount;
+        //--.
+        if(sufficientFunds)
+        {
+            accBal -= amount;
+        }
+        //--.
+        return sufficientFunds;
+    }
+
+    //--.
+    public void TransferFrom(BankAccount accFrom, decimal amount) 
+    { 
+        //--.
+        if( accFrom.Withdraw(amount) )
+        {
+            this.Deposit(amount);
+        }
+    } 
+
+}
---
+++ Lab09/Starter/ConstructorsBankTransactions/ConstructorsBankTransactions/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConstructorsSpace
+{
+    class Test
+    {
+        //--.
+        static void Main()
+        {
+            BankAccount acc1, acc2, acc3, acc4;
+
+            acc1 = new BankAccount();
+            acc2 = new BankAccount(AccountType.Deposit);
+            acc3 = new BankAccount(100);
+            acc4 = new BankAccount(AccountType.Deposit, 500);
+
+            //--.
+            Write(acc1);
+            Write(acc2);
+            Write(acc3);
+            Write(acc4);
+        }
+
+        //--.
+        static void Write(BankAccount acc) 
+        {
+            Console.WriteLine("Account number is {0}", acc.Number() );
+            Console.WriteLine("Account number is {0}", acc.Ballance());
+            Console.WriteLine("Account type is {0}", acc.Type());
+            
+        }
+    }
+}
